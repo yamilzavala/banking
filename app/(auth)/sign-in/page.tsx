e4c006b22830a6ed0fd0b01afadad04e7dfbe945
yazavala_meli <yamil.zavala@mercadolibre.com>
2025-06-07 13:32:30 -0300

    implement initial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      SignIn
+    </section>
+  )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      SignUp
+    </section>
+  )
+}
+
+export default SignUp
